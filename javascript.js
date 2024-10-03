b0be164730117af56eefb4611a23bba7d797baee
gregoriusjeasenridzal@gmail.com <gregoriusjeasenridzal@gmail.com>
2024-10-03 21:52:17 +0800

    Add basic files and game functionality
    Add index.html and javascript.js, also add basic game functionality to the rock-paper-scissors game (without scores yet).

---
+++ javascript.js
@@ -0,0 +1,42 @@
+function getComputerChoice () {
+    let rng = Math.random();
+    return rng < (1 / 3) ? "rock":
+    rng < (2 / 3) ? "paper":
+    "scissors";
+    
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    return prompt("Rock, Paper, or Scissors?");
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice == "rock" && computerChoice == "rock" ? alert("Tie!") :
+    humanChoice == "rock" && computerChoice == "paper" ? alert("Lose!") && ++computerScore && console.log(computerScore) :
+    humanChoice == "rock" && computerChoice == "scissors" ? alert ("Win!") && ++humanScore && console.log(humanScore) :
+    humanChoice == "paper" && computerChoice == "rock" ? alert("Win!") && ++humanScore && console.log(humanScore) :
+    humanChoice == "paper" && computerChoice == "paper" ? alert("Tie!") :
+    humanChoice == "paper" && computerChoice == "scissors" ? alert ("Lose!") && ++computerScore && console.log(computerScore) :
+    humanChoice == "scissors" && computerChoice == "rock" ? alert("Tie!") && ++computerScore && console.log(computerScore) :
+    humanChoice == "scissors" && computerChoice == "paper" ? alert("Lose!") && ++humanScore && console.log(humanScore) :
+    humanChoice == "scissors" && computerChoice == "scissors" ? alert ("Win!") :
+    console.log("wtf happened to the code");
+}
+
+const preHumanChoice = getHumanChoice();
+const humanChoice = preHumanChoice.toLowerCase();
+const computerChoice = getComputerChoice();
+
+console.log(humanChoice)
+
+playRound(humanChoice, computerChoice)
+
+console.log(humanScore)
+console.log(computerScore)
+
+
