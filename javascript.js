520f3b065101302c02bd0ba9b74ff96e5786962f
gregoriusjeasenridzal@gmail.com <gregoriusjeasenridzal@gmail.com>
2024-10-04 15:06:29 +0800

    Add gameplay loop and core functions
    Add function updateChoice to return updated choices instead of using old choices when starting new rounds, and added gameplay loop that lasts 5 rounds.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+function playGame(){
+
 function getComputerChoice () {
     let rng = Math.random();
     return rng < (1 / 3) ? "rock":
@@ -6,31 +8,37 @@ function getComputerChoice () {
     
 }
 
-
-
 function getHumanChoice() {
     return prompt("Rock, Paper, or Scissors?");
+}
+
+function updateChoice() {
+    preHumanChoice = getHumanChoice();
+    humanChoice = preHumanChoice.toLowerCase();
+    computerChoice = getComputerChoice();
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    humanChoice == "rock" && computerChoice == "rock" ? alert("Tie!") :
-    humanChoice == "rock" && computerChoice == "paper" ? ++computerScore && alert("Lose!") :
-    humanChoice == "rock" && computerChoice == "scissors" ? ++humanScore && alert ("Win!") :
-    humanChoice == "paper" && computerChoice == "rock" ? ++humanScore && alert("Win!") :
-    humanChoice == "paper" && computerChoice == "paper" ? alert("Tie!") :
-    humanChoice == "paper" && computerChoice == "scissors" ? ++computerScore && alert ("Lose!") :
-    humanChoice == "scissors" && computerChoice == "rock" ? ++computerScore && alert("Lose!") :
-    humanChoice == "scissors" && computerChoice == "paper" ? ++humanScore && alert("Win!") :
-    humanChoice == "scissors" && computerChoice == "scissors" ? alert ("Tie!") :
+    humanChoice == "rock" && computerChoice == "rock" ? alert("Tie! R=R") :
+    humanChoice == "rock" && computerChoice == "paper" ? ++computerScore && alert("Lose! R<P") :
+    humanChoice == "rock" && computerChoice == "scissors" ? ++humanScore && alert ("Win! R>S") :
+    humanChoice == "paper" && computerChoice == "rock" ? ++humanScore && alert("Win! P>R") :
+    humanChoice == "paper" && computerChoice == "paper" ? alert("Tie! P=P") :
+    humanChoice == "paper" && computerChoice == "scissors" ? ++computerScore && alert ("Lose! P<S") :
+    humanChoice == "scissors" && computerChoice == "rock" ? ++computerScore && alert("Lose! S<R") :
+    humanChoice == "scissors" && computerChoice == "paper" ? ++humanScore && alert("Win! S>P ") :
+    humanChoice == "scissors" && computerChoice == "scissors" ? alert ("Tie! S=S") :
     console.log("wtf happened to the code");
 }
 
-const preHumanChoice = getHumanChoice();
-const humanChoice = preHumanChoice.toLowerCase();
-const computerChoice = getComputerChoice();
+// below is the current core gameplay loop
+
+let preHumanChoice = getHumanChoice();
+let humanChoice = preHumanChoice.toLowerCase();
+let computerChoice = getComputerChoice();
 
 console.log(humanChoice)
 console.log(computerChoice)
@@ -40,4 +48,38 @@ playRound(humanChoice, computerChoice)
 console.log(`your score is ${humanScore}`);
 console.log(`the computer score is ${computerScore}`)
 
+updateChoice();
+console.log(humanChoice);
+console.log(computerChoice);
+playRound(humanChoice, computerChoice);
 
+console.log(`your score is ${humanScore}`);
+console.log(`the computer score is ${computerScore}`)
+
+updateChoice();
+console.log(humanChoice);
+console.log(computerChoice);
+playRound(humanChoice, computerChoice);
+
+console.log(`your score is ${humanScore}`);
+console.log(`the computer score is ${computerScore}`)
+
+updateChoice();
+console.log(humanChoice);
+console.log(computerChoice);
+playRound(humanChoice, computerChoice);
+
+console.log(`your score is ${humanScore}`);
+console.log(`the computer score is ${computerScore}`)
+
+updateChoice();
+console.log(humanChoice);
+console.log(computerChoice);
+playRound(humanChoice, computerChoice);
+
+console.log(`your score is ${humanScore}`);
+console.log(`the computer score is ${computerScore}`)
+
+}
+
+playGame()
